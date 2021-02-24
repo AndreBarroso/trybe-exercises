5561feef7d7705bb93bde9e2375dd097d40babda
AndreBarroso <andre_barroso17@hotmail.com>
2021-02-24 19:35:18 -0300

    Questao 5

---
+++ 1-Fundamentos-de-Desenvolvimento-Web/BLOCO-04-Introducao-JavaScript-e-Lógica-de-Programacao/Dia_1/5_retornaTrueFalse.js
@@ -0,0 +1,16 @@
+let a = 90;
+let b = 30;
+let c = 60;
+
+let soma = a + b + c;
+let resultado;
+
+if(soma===180){
+    resultado = true;
+}
+
+else{
+    resultado = false;
+}
+
+console.log(resultado);
