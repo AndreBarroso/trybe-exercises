e1e8fdcef89ebd76683fb422eff83e6f37cf3227
AndreBarroso <andre_barroso17@hotmail.com>
2021-02-24 18:56:41 -0300

    Questao 1

---
+++ 1-Fundamentos-de-Desenvolvimento-Web/BLOCO-04-Introducao-JavaScript-e-Lógica-de-Programacao/Dia_1/1-2_subtracao.js
@@ -0,0 +1,8 @@
+/*Programa para imprimir subtração entre a e b*/
+let a = 3;
+let b = 2;
+let subtracao;
+
+subtracao = a - b;
+
+console.log(subtracao);
---
+++ 1-Fundamentos-de-Desenvolvimento-Web/BLOCO-04-Introducao-JavaScript-e-Lógica-de-Programacao/Dia_1/1-5_modulo.js
@@ -0,0 +1,8 @@
+/*Programa para imprimir o resto da divisão entre a e b*/
+let a = 9;
+let b = 5;
+let modulo;
+
+modulo  = a % b;
+
+console.log(modulo);
