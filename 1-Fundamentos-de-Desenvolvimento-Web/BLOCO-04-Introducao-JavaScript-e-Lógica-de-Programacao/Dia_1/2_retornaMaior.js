c9378da0b6c8db77dca276b4e04aab211fccb5bf
AndreBarroso <andre_barroso17@hotmail.com>
2021-02-24 19:02:21 -0300

    Questao 2 resolvida

---
+++ 1-Fundamentos-de-Desenvolvimento-Web/BLOCO-04-Introducao-JavaScript-e-Lógica-de-Programacao/Dia_1/2_retornaMaior.js
@@ -0,0 +1,17 @@
+/*Programa para imprime o maior entre a e b*/
+let a = 10;
+let b = 60;
+let resultado;
+
+if(a>b){
+    console.log(a);
+}
+
+else if(b>a){
+    console.log(b);
+}
+
+else{
+    console.log("Ambos tem o mesmo valor.")
+}
+
