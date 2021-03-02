ad20bd803aaea58b1d518a78c003ffde031a0aff
AndreBarroso <andre_barroso17@hotmail.com>
2021-03-01 21:04:35 -0300

    Revert "Resoluçao dos exercicios de objetos"
    This reverts commit 0da8bc107a28c8b005995ca9bbb18a2deafed536.

---
+++ 1-Fundamentos-de-Desenvolvimento-Web/BLOCO-04-Introducao-JavaScript-e-Lógica-de-Programacao/Dia_4/objetos.js
@@ -0,0 +1,25 @@
+var p='arara';
+
+console.log(p.length);
+/* Exercicio 1 */
+function verificaPalindrome(palavra){
+    if(palavra.length%2===0){
+        return false;
+    }
+    for
+}
+console.log('');
+
+/* Exercicio 2 */
+
+console.log('');
+
+/* Exercicio 3 */
+
+console.log('');
+
+/* Exercicio 4 */
+
+console.log('');
+
+/* Exercicio 5 */
