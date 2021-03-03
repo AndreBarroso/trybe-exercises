2a0fc821bb22f90f2a61b8a7e823881e1f0109fa
AndreBarroso <andre_barroso17@hotmail.com>
2021-03-03 19:01:38 -0300

    Resolvida questao 5

--- 1-Fundamentos-de-Desenvolvimento-Web/BLOCO-04-Introducao-JavaScript-e-Lógica-de-Programacao/Dia_4/funcoes.js
+++ 1-Fundamentos-de-Desenvolvimento-Web/BLOCO-04-Introducao-JavaScript-e-Lógica-de-Programacao/Dia_4/funcoes.js
@@ -1,25 +1,84 @@
-var p='arara';
+var word='arara';
 
-console.log(p.length);
 /* Exercicio 1 */
 function verificaPalindrome(palavra){
+    var i=0;
     if(palavra.length%2===0){
         return false;
     }
-    for
+    for(i=0; i<palavra.length/2; i+=1){
+        if(palavra[i]!==palavra[palavra.length-i-1]){
+            return false;
+        }
+    }
+    return true;
 }
+console.log(verificaPalindrome(word));
 console.log('');
+
 
 /* Exercicio 2 */
-
+function retornaMaior(array){
+    let maior = 0;
+    let index;
+    for(let i=0; i<array.length; i+=1){
+        if(array[i]>maior){
+            maior=array[i];
+            index=i;
+        }
+    }
+    return index;
+}
+console.log(retornaMaior([2, 3, 6, 7, 10, 1]));
 console.log('');
+
 
 /* Exercicio 3 */
-
+function retornaMenor(array){
+    let index = 0;
+    for(let i=0; i<array.length; i+=1){
+        if(array[i]<array[index]){
+            index=i;
+        }
+    }
+    return index;
+}
+console.log(retornaMenor([2, 4, 6, 7, 10, 0, -3]));
 console.log('');
 
-/* Exercicio 4 */
 
+/* Exercicio 4 */
+function retornaMaiorNome(nome){
+    let maiorNome = nome[0];
+    for(indice in nome){
+        if(nome[indice].length>maiorNome.length ){
+            maiorNome =nome[indice];
+        }
+    }
+    return maiorNome ;
+}
+console.log(retornaMaiorNome(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
 console.log('');
 
 /* Exercicio 5 */
+function maisRepetido(array){
+    let resultado;
+    let contador = 0;;
+    let maiorContagem=0;
+    let valorVerificado;
+    for(let i=0; i<array.length; i+=1){
+        valorVerificado=array[i];
+        contador=0;
+        for(var j = 0; j < array.length; j ++){
+            if(array[j]===array[i] && j!==i){
+                contador+=1;
+            }
+        }
+        if(contador > maiorContagem){
+            maiorContagem = contador;
+            resultado = array[i];
+        }
+    }
+    return resultado;
+}
+console.log(maisRepetido( [2, 1, 1, 1, 8, 2, 3]));
--- 1-Fundamentos-de-Desenvolvimento-Web/BLOCO-04-Introducao-JavaScript-e-Lógica-de-Programacao/Dia_4/objetos.js
+++ 1-Fundamentos-de-Desenvolvimento-Web/BLOCO-04-Introducao-JavaScript-e-Lógica-de-Programacao/Dia_4/objetos.js
@@ -1,25 +1,38 @@
-var p='arara';
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
 
-console.log(p.length);
 /* Exercicio 1 */
-function verificaPalindrome(palavra){
-    if(palavra.length%2===0){
-        return false;
-    }
-    for
-}
+console.log('Bem vinda '+info.personagem+'!');
 console.log('');
 
 /* Exercicio 2 */
-
+info['recorrente']='Sim';
+console.log(info);
 console.log('');
 
 /* Exercicio 3 */
-
+for(var chaves in info){
+    console.log(chaves);
+}
 console.log('');
 
 /* Exercicio 4 */
-
+console.log('');
+for(var chaves in info){
+    console.log(info[chaves]);
+}
+console.log('');
 console.log('');
 
 /* Exercicio 5 */
+var info2 ={};
+arrInfo=['Margarida e tio patinhas','Pato Donald e Christmas on Bear Mountain, Dell`s Four Color Comics #178','Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas','Ambos recorrentes // Atenção para essa última linha!']
+let i=0;
+for(var chaves in info){
+    info2[chaves]=arrInfo[i];
+    i+=1;
+    console.log(info2[chaves]);
+}
