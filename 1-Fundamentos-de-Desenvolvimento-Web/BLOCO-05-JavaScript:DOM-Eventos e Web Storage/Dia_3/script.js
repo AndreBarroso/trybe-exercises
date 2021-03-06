f7953a05a96d07744f5ccdc1a8c5c3c897464caf
AndreBarroso <andre_barroso17@hotmail.com>
2021-03-06 09:32:29 -0300

    exercicio 3

--- 1-Fundamentos-de-Desenvolvimento-Web/BLOCO-05-JavaScript:DOM-Eventos e Web Storage/Dia_3/script.js
+++ 1-Fundamentos-de-Desenvolvimento-Web/BLOCO-05-JavaScript:DOM-Eventos e Web Storage/Dia_3/script.js
@@ -13,15 +13,43 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
 
+  //function createDiaCalendario ()
+
   let listaDias = document.querySelector('#days');
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
   
   for( index = 0; index < dezDaysList.length; index +=1 ) {
     let addDay = document.createElement('li');
     addDay.innerText = dezDaysList[index];
+    if(addDay.innerText === '24' || addDay.innerText === '25' || addDay.innerText === '31' ) {
+        addDay.className = 'holiday';
+    }
     listaDias.appendChild(addDay);
 }
 
-  //function createDiaCalendario ()
-  
-  
+
+function addBotHoliday (Feriados) {
+    let btnsContainer = document.querySelector('.buttons-container');
+    let buttonHoliday = document.createElement('button');
+    buttonHoliday.innerText = Feriados;
+    buttonHoliday.id = 'btn-holiday';
+    btnsContainer.appendChild(buttonHoliday);
+}
+
+function changeColorHolidaysDays(){ 
+    let btnsContainer = document.querySelector('.buttons-container');
+    let daysHoliday = document.querySelectorAll('.holiday');
+    btnsContainer.addEventListener('click', function(){
+        for(let i = 0; i < daysHoliday.length; i += 1){
+            if(daysHoliday[i].style.backgroundColor !== 'white') {
+                daysHoliday[i].style.backgroundColor = 'white';
+            } else if (daysHoliday[i].style.backgroundColor === 'white') {
+                daysHoliday[i].style.backgroundColor = 'rgb(238,238,238)';
+            }
+        }
+    });
+        
+}
+
+addBotHoliday('Feriados');
+changeColorHolidaysDays();
