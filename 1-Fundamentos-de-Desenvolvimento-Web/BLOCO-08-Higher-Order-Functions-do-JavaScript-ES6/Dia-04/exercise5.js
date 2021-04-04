e8442c85feafce9acb6a1854fbb160892c16c7e0
AndreBarroso <andre_barroso17@hotmail.com>
2021-04-04 19:54:25 -0300

    Exercicios 4 e 5

---
+++ 1-Fundamentos-de-Desenvolvimento-Web/BLOCO-08-Higher-Order-Functions-do-JavaScript-ES6/Dia-04/exercise5.js
@@ -0,0 +1,22 @@
+
+const assert = require('assert');
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+  // escreva seu código aqui
+  let sum = 0;
+  const ret =  names.reduce((acumulado, nome) => acumulado + nome).toLocaleLowerCase().split('');
+  ret.forEach(element => {
+      if(element === 'a') sum += 1;
+  });
+
+  return sum;
+}
+console.log(containsA());
+
+//assert.deepStrictEqual(containsA(), 20);
