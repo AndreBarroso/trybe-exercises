3d51568a2e29644f2c9793aca922a5728c0a9448
AndreBarroso <andre_barroso17@hotmail.com>
2021-04-12 19:19:30 -0300

    Exercicio 5

--- 1-Fundamentos-de-Desenvolvimento-Web/BLOCO-09-JavaScript-Assincrono-e-Promisses/Dia-01/exercise5.js
+++ 1-Fundamentos-de-Desenvolvimento-Web/BLOCO-09-JavaScript-Assincrono-e-Promisses/Dia-01/exercise5.js
@@ -5,12 +5,14 @@ const getMarsTemperature = () => {
   return Math.floor(Math.random() * maxTemperature);
 };
 
-// crie a função sendMarsTemperature abaixo
-const sendMarsTemperature = () => {
-    setTimeout(() => {
-        console.log(`Mars temperatur is: ${getMarsTemperature()} degree Celsius`)
-    },messageDelay())
-}
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
+const sendMarsTemperature = (onSuccess) => {
+    const currentTemperature = getMarsTemperature();
+    setTimeout(() => onSuccess(currentTemperature), messageDelay());
+};
 
-sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+sendMarsTemperature(temperatureInFahrenheit);
+sendMarsTemperature(greet);
