29f35ad2ebf5be7ae2fb49d7861828ad728636a2
AndreBarroso <andre_barroso17@hotmail.com>
2021-04-20 13:58:52 -0300

    Exercicio 1

--- 1-Fundamentos-de-Desenvolvimento-Web/BLOCO-10-Testes-Automatizados-Com-Jest/Dia-01/parte1/sum.test.js
+++ 1-Fundamentos-de-Desenvolvimento-Web/BLOCO-10-Testes-Automatizados-Com-Jest/Dia-01/parte1/sum.test.js
@@ -1,5 +1,14 @@
+const { test, expect } = require('@jest/globals');
 const sum  = require('./sum');
 
 describe('Função sum', () => {
-    test('Soma de 2 + 2 = 4', () => expect(sum(2,2)).toBe(4));
+    test('Soma de 2 + 2 = 4', () => expect(sum(2, 2)).toBe(4));
+    test('Soma de 0 + 0 = 0', () => expect(sum(0, 0)).toBe(0));
+    test('Retorna error se receber uma string', () => 
+        expect(() => {sum(1, '1')}).toThrow()
+    );
+    test('Retorna error com a (frase parameters must be numbers) se receber uma string', () => 
+        expect(() => {sum(1, '1')}).toThrowError(new Error('parameters must be numbers'))
+    );
+    
 });
