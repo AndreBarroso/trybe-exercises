11735bdfe6b1e53a0556c15fa997509142628529
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-12 16:14:33 -0300

    feat: exercises  mongo updates 1 to 4

---
+++ 3-Desenvolvimento-Back-end/Bloco-24-MongoDB-Updates-Simples-e-Complexos/Dia-01/exercicios_do_dia.mjs
@@ -0,0 +1,21 @@
+
+// Exercícios do dia
+
+/* Exercício 1 : Altere o imdbRating para 7.7 no filme Batman */
+db.movies.updateOne({"title": "Batman"}, {$set: {imdbRating: 7.7} });
+
+/* Exercício 2 : Altere budget para 1 no filme Godzilla . */
+db.movies.updateOne({title: "Godzila"}, {$set: {budget: 1}});
+
+/* Exercício 3 : Altere budget para 15 e imdbRating para 5.5 no filme Home Alone . */
+db.movies.updateOne(
+  {title: "Alone"},
+  {$set: {budget: 15, imdbRating: 5.5}}
+);
+
+/* Exercício 4 : Aumente em 2 o imdbRating do filme Batman . */
+db.movies.updateOne(
+  {title: "Batman"},
+  {$inc: {imdbRating: 2}}
+);
+
