c0e5911fecde1d34b5937f77cb86cd2d1366be6e
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-12 18:53:59 -0300

    feat: exercise  mongo updates 12

--- 3-Desenvolvimento-Back-end/Bloco-24-MongoDB-Updates-Simples-e-Complexos/Dia-01/exercicios_do_dia.mjs
+++ 3-Desenvolvimento-Back-end/Bloco-24-MongoDB-Updates-Simples-e-Complexos/Dia-01/exercicios_do_dia.mjs
@@ -64,7 +64,6 @@ db.movies.updateOne(
   }
 );
 
-
 /* Exercício 11 : Utilizando uma única operação, crie um campo chamado
 sequels e atribua a ele o valor 0 em todos os documentos. */
 db.movies.updateMany({},
@@ -73,7 +72,14 @@ db.movies.updateMany({},
 
 /* Exercício 12 : Utilizando uma única operação, remova os campos budget
 e estimatedBudget em todos os documentos. */
-
+db.movies.updateMany({},
+  {
+      $unset: {
+      "budget": "",
+      "estimatedBudget": "",
+    }
+  },
+);
 
 /* Exercício 13 : Para os filmes Batman ou Home Alone , atribua a imdbRating
 o valor 17 , caso o valor de imdbRating seja menor que 17 . */
