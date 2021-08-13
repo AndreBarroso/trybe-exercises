c5dc5ded531baf8360031d349ade0fe5a291d639
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-13 13:09:33 -0300

    feat: exercise bonus  mongo updates complexos 4

--- 3-Desenvolvimento-Back-end/Bloco-24-MongoDB-Updates-Simples-e-Complexos/Dia-02/ExerciciosUptadesComplexosMongoDB.js
+++ 3-Desenvolvimento-Back-end/Bloco-24-MongoDB-Updates-Simples-e-Complexos/Dia-02/ExerciciosUptadesComplexosMongoDB.js
@@ -29,6 +29,10 @@ db.movies.updateOne(
 
 /* Exercício 4: Remova o primeiro elemento do array category do filme
 Batman . */
+db.movies.updateOne(
+  {title: "Batman"},
+  {$pop: {category: -1}},
+);
 
 /* Exercício 5: Remova o último elemento do array category do filme Batman . */
 
