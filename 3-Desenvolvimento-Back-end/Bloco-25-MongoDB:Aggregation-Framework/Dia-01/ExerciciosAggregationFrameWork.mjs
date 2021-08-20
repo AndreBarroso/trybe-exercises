8f2ff8f2713104a606856d431c50b6909c4ded92
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-20 11:28:54 -0300

    feat:  exercicio 13  mongo Aggregation Framework parte1

--- 3-Desenvolvimento-Back-end/Bloco-25-MongoDB:Aggregation-Framework/Dia-01/ExerciciosAggregationFrameWork.mjs
+++ 3-Desenvolvimento-Back-end/Bloco-25-MongoDB:Aggregation-Framework/Dia-01/ExerciciosAggregationFrameWork.mjs
@@ -262,7 +262,7 @@ db.vendas.aggregate([
   },
   { $limit: 3 },
   {
-project: {
+    $project: {
       _id: 0,
       uf: "$_id",
       totalVendas: 1
@@ -273,3 +273,45 @@ project: {
 /* Exercício 13 : Encontre qual foi o total de vendas e a média
  de vendas de cada uf no ano de 2019 . Ordene os resultados pelo
 nome da uf . Retorne os documentos no seguinte formato: */
+
+db.vendas.aggregate([
+  {
+    match: {
+      dataVenda: {
+        $gte: ISODate('2020-01-01')
+      }
+    }
+  },
+  {
+    $lookup: {
+      from: 'clientes',
+      localField: 'clienteId',
+      foreignField: 'clienteId',
+      as: 'dadosCliente'
+    }
+  },
+  // {
+  //   $unwind: "$dadosCliente"
+  // },
+  // {
+  //   $group: {
+  //     _id: "$dadosCliente.endereco.uf",
+  //     totalVendas: {
+  //       sum: 1
+  //     }
+  //   }
+  // },
+  // {
+  //   $sort: {
+  //     totalVendas: -1
+  //   }
+  // },
+  // { $limit: 3 },
+  // {
+  //   $project: {
+  //     _id: 0,
+  //     uf: "$_id",
+  //     totalVendas: 1
+  //   }
+  // }
+]);
