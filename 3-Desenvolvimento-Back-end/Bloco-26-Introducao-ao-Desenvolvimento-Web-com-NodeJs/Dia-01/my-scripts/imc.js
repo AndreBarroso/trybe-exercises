bad32e75b1fa1803914af508fbc09a2d9abd95a9
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-23 15:41:04 -0300

    feat: exercise 9 introduçao a node

--- 3-Desenvolvimento-Back-end/Bloco-26-Introducao-ao-Desenvolvimento-Web-com-NodeJs/Dia-01/my-scripts/imc.js
+++ 3-Desenvolvimento-Back-end/Bloco-26-Introducao-ao-Desenvolvimento-Web-com-NodeJs/Dia-01/my-scripts/imc.js
@@ -1,10 +1,20 @@
 const readline = require('readline-sync');
 
-function calculaIMC(peso, altura) {
-  return peso/(altura*altura)
+function calculaIMC(pes, alt) {
+  return pes/(alt*alt);
 }
 
+function healthSituation(func, pes, alt) {
+  const imc = func(pes, alt);
+  console.log('Seu IMC é: ', imc)
+  if(imc < 18.5) console.log('Abaixo do peso (magreza)');
+  if(imc >= 18.5 && imc <= 24.9) console.log('Peso normal');
+  if(imc >= 25 && imc <= 29.9) console.log('Acima do peso (sobrepeso)');
+  if(imc >= 30 && imc <= 34.9) console.log('Obesidade grau I');
+  if(imc >= 35 && imc <= 39.9) console.log('Obesidade grau I');
+  if(imc > 40) console.log('Obesidade graus III e IV');
+}
 const peso = readline.questionFloat('Qual seu peso em kg ');
 const altura = readline.questionFloat('Qual sua altura em metros? ');
 
-console.log(calculaIMC(peso, altura))
+healthSituation(calculaIMC, peso, altura)
