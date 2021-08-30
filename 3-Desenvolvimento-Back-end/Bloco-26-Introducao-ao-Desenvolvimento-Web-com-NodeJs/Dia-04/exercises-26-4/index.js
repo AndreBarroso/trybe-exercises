8555cc76eacfe7662dcda5b23185996b2c8acb35
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-30 11:03:43 -0300

    feat: exercicios node.js, introducao a express -> exercicio 3

--- 3-Desenvolvimento-Back-end/Bloco-26-Introducao-ao-Desenvolvimento-Web-com-NodeJs/Dia-04/exercises-26-4/index.js
+++ 3-Desenvolvimento-Back-end/Bloco-26-Introducao-ao-Desenvolvimento-Web-com-NodeJs/Dia-04/exercises-26-4/index.js
@@ -6,6 +6,7 @@ app.use(bodyParser.json());
 
 const obj =  { message: 'pong'};
 const messages = [];
+const greet = [];
 
 app.get('/ping', (req, res) => {
   res.status(200).json(obj);
@@ -18,10 +19,21 @@ app.post('/hello', (req, res) => {
   res.status(200).json({ "message": `Hello, ${name}!` })
 })
 
-
 app.get('/hello', (req, res) => {
   res.status(200).json(messages);
 });
+
+app.post('/greeting', (req, res) => {
+  const {name, age } = req.body;
+  if( age <= 17 ) return res.status(401).json({ "message": "Unauthorized" });
+  greet.push({ message: `Hello, ${name}!`, age })
+  res.status(200).json(greet);
+});
+
+app.get('/greeting', (req, res) => {
+  res.status(200).json(greet);
+});
+
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo a porta 3001');
