49c03665f157284d60ddc8e28967c63a7b6f97b5
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-30 10:48:52 -0300

    feat: exercicios node.js, introducao a express -> exercicio 2

--- 3-Desenvolvimento-Back-end/Bloco-26-Introducao-ao-Desenvolvimento-Web-com-NodeJs/Dia-04/exercises-26-4/index.js
+++ 3-Desenvolvimento-Back-end/Bloco-26-Introducao-ao-Desenvolvimento-Web-com-NodeJs/Dia-04/exercises-26-4/index.js
@@ -5,9 +5,22 @@ const app = express();
 app.use(bodyParser.json());
 
 const obj =  { message: 'pong'};
+const messages = [];
 
 app.get('/ping', (req, res) => {
   res.status(200).json(obj);
+});
+
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  messages.push({ "message": `Hello, ${name}!` });
+  res.status(200).json({ "message": `Hello, ${name}!` })
+})
+
+
+app.get('/hello', (req, res) => {
+  res.status(200).json(messages);
 });
 
 app.listen(3001, () => {
