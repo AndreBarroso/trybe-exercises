949bead4db10912b8970b6225d15fdf7d9fc8c5a
AndreBarroso <andre_barroso17@hotmail.com>
2021-09-02 19:28:33 -0300

    feat: exercicios bloco 27.1 - conteudo dia

---
+++ 3-Desenvolvimento-Back-end/Bloco-27-NodeJS:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/Dia-01/model-example-mongo/models/Author.js
@@ -0,0 +1,75 @@
+// models/Authors.js
+const { ObjectId } = require('mongodb');
+
+const connection = require('./connection');
+
+// Cria uma string com o nome completo do autor
+
+const getNewAuthor = (authorData) => {
+const { id, firstName, middleName, lastName } = authorData;
+
+const fullName = [firstName, middleName, lastName]
+ .filter((name) => name)
+ .join(' ');
+
+return {
+ id,
+ firstName,
+ middleName,
+ lastName,
+ name: fullName,
+};
+};
+
+// Serializa o nome dos campos de snake_case para camelCase
+
+const serialize = (authorData) => ({
+ id: authorData.id,
+ firstName: authorData.first_name,
+ middleName: authorData.middle_name,
+ lastName: authorData.last_name,
+});
+
+// Busca todos os autores do banco.
+
+const getAll = async () => {
+  return connection()
+      .then((db) => db.collection('authors').find().toArray())
+          .then((authors) =>
+              authors.map(({ _id, firstName, middleName, lastName }) =>
+              getNewAuthor({
+                  id: _id,
+                  firstName,
+                  middleName,
+                  lastName,
+              })
+          )
+      );
+}
+
+/*
+Busca um autor específico, a partir do seu ID
+@param {String} id ID do autor a ser recuperado
+*/
+
+const findById = async (id) => {
+    if (!ObjectId.isValid(id)) {
+        return null;
+    }
+
+    const authorData = await connection()
+        .then((db) => db.collection('authors').findOne(new ObjectId(id)));
+
+    if (!authorData) return null;
+
+    const { firstName, middleName, lastName } = authorData;
+
+    return getNewAuthor({ id, firstName, middleName, lastName });
+};
+  
+  module.exports = {
+   // isValid,
+    getAll,
+    findById,
+    //create,
+  };
