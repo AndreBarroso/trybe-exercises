5cf2a6363c3c29856ef62f789f2520a3f13d8f34
AndreBarroso <andre_barroso17@hotmail.com>
2021-10-08 17:02:00 -0300

    feat: criado migration create-books

--- 3-Desenvolvimento-Back-end/Bloco-30-Arquitetura Solid e ORM/Dia-02/library_exercise_sequelize/migrations/20211008192212-create-books.js
+++ 3-Desenvolvimento-Back-end/Bloco-30-Arquitetura Solid e ORM/Dia-02/library_exercise_sequelize/migrations/20211008192212-create-books.js
@@ -1,21 +1,37 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: async (queryInterface, DataTypes) => {
+   return queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      author: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      pageQuantity: {
+        allowNull: false,
+        type: DataTypes.INTERGER,
+      },
+      pageQuantity: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+
+   }
+
+   )
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    return queryInterface.dropTable('Books');
   }
 };
