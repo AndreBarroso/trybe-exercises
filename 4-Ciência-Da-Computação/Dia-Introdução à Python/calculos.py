0b49c7eb4937a9006f91f848f569efbd61f003c8
AndreBarroso <andre_barroso17@hotmail.com>
2021-12-02 15:20:15 -0300

    feat: exercicio2 python

--- 4-Ciência-Da-Computação/Dia-Introdução à Python/calculos.py
+++ 4-Ciência-Da-Computação/Dia-Introdução à Python/calculos.py
@@ -1,6 +1,16 @@
 def maior(num1, num2):
-  if num1 > num2:
-    return num1
-  return num2
+    if num1 > num2:
+        return num1
+    return num2
 
-print(maior(2, 4))
+
+lista = [2, 3, 5, 1]
+
+def media(lista):
+    soma = 0
+    for n in lista:
+        soma = soma + n
+    return soma/len(lista)
+
+print(media(lista))
+
