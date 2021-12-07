52874898a9d9a4b1736dec4b1fc0ac3b97544ae1
AndreBarroso <andre_barroso17@hotmail.com>
2021-12-07 19:16:46 -0300

    feat: exer1 dia 2 POO

---
+++ 4-Ciência-Da-Computação/Bloco-33-Programação-Orientada-a-Objetos/Dia-01/exercicio2.py
@@ -0,0 +1,14 @@
+from _typeshed import Self
+
+
+class User:
+    def __init__(self, lado_a, lado_b):
+        self.lado_a = lado_a
+        self.lado_b = lado_b
+
+    def area(self):
+        area = self.lado_a * self.lado_a
+        
+        
+
+   
